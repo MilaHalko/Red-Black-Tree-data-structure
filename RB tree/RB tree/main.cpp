ef13800e7fcdd4b91012aacb6c4fe57acbb2fef5
milka_deluxe <milagalko1@gmail.com>
2021-10-19 02:19:13 +0300

    rbtree insert in process

--- RB tree/RB tree/main.cpp
+++ RB tree/RB tree/main.cpp
@@ -1,4 +1,5 @@
 #include "library.hpp"
+
 enum Colors {
     BLACK,
     RED
@@ -9,13 +10,15 @@ class Node {
     string data;
     bool color;     // 0 - black    1 - red
     Node *lChild,
-         *rChild;
+         *rChild,
+         *parent;
     
-    Node(int key = -1, string data = "", bool color = RED, Node *lChild = nullptr, Node *rChild = nullptr ) {
+    Node(int key = -1, string data = "", Node *parent = nullptr, bool color = RED, Node *lChild = nullptr, Node *rChild = nullptr ) {
         this->key = key;
         this->data = data;
         this->color = color;
         this->lChild = lChild;
+        this->parent = parent;
         this->rChild = rChild;
     };
     
@@ -30,11 +33,14 @@ public:
     void insert(int, string);
     void prettyPrint();
     void printHelper(Node*, string, bool);
+    bool isLChild(Node*, Node*);
+    void leftRotate(Node*);     // Node which goes up
+    void rightRotate(Node*);    //
 };
 
 void RBTree::insert(int key, string data = "") {
     if (root == nullptr) {
-        root = new Node(key, data, BLACK);
+        root = new Node(key, data, nullptr, BLACK);
     }
     else {
         Node* current = root;
@@ -45,10 +51,101 @@ void RBTree::insert(int key, string data = "") {
             key < current->key ? current = current->lChild : current = current->rChild;
         }
         
-        current = new Node(key, data);
+        current = new Node(key, data, prev);
         prev->key > current->key ? prev->lChild = current : prev->rChild = current;
+        
+        if (prev->color == RED) {
+            Node* uncle;
+            isLChild(prev, prev->parent) ? uncle = prev->parent->rChild : uncle = prev->parent->lChild;
+            
+            if (uncle != nullptr  &&  uncle->color == RED) {
+                uncle->color = prev->color = BLACK;
+                prev->parent != root ? prev->parent->color = RED : prev->parent->color = BLACK;
+            }
+            else {
+                
+                if (!isLChild(prev, prev->parent)) {
+                    if (isLChild(current, prev)) {
+                        rightRotate(current);
+                        leftRotate(current);
+                        current->color = BLACK;
+                        current->lChild->color = RED;
+                    }
+                    else {
+                        leftRotate(prev);
+                        current->parent->color = BLACK;
+                        current->parent->lChild->color = RED;
+                    }
+                }
+                
+                else {
+                    if (!isLChild(current, prev)) {
+                        leftRotate(current);
+                        rightRotate(current);
+                        current->color = BLACK;
+                        current->rChild->color = RED;
+                    }
+                    else {
+                        leftRotate(prev);
+                        current->parent->color = BLACK;
+                        current->parent->rChild->color = RED;
+                    }
+                }
+            }
+        }
     }
+    root->color = BLACK;
+}
+
+void RBTree::rightRotate(Node* node) {
+    Node* G = node->parent->parent; // G - future grand (used to be parent's parent)
+    Node* C = node->parent;         // C - future right child (used to be parent)
+    Node* currRChild = node->rChild;
     
+    G->rChild = node;
+    node->parent = G;
+    
+    C->parent = node;
+    node->rChild = C;
+    
+    Node* current = node->rChild;
+    while (current != nullptr) {
+        current = current->lChild;
+    }
+    current = currRChild;
+    currRChild->parent = current->parent;
+}
+
+void RBTree::leftRotate(Node* node) {
+    Node* G = node->parent->parent; // G - future grand (used to be parent's parent)
+    Node* C = node->parent;         // C - future left child (used to be parent)
+    Node* currLChild = node->lChild;
+    
+    G->rChild = node;
+    node->parent = G;
+    
+    C->parent = node;
+    node->lChild = C;
+    
+    Node* current = node->lChild;
+    while (current != nullptr) {
+        current = current->rChild;
+    }
+    current = currLChild;
+    currLChild->parent = current->parent;
+}
+
+bool RBTree::isLChild(Node* current, Node* prev) {
+    bool IsLeftChild;
+    if (prev->key > current->key) {
+        IsLeftChild = true;
+        prev->lChild = current;
+    }
+    else {
+        IsLeftChild = false;
+        prev->rChild = current;
+    }
+    return IsLeftChild;
 }
 
 void RBTree::prettyPrint() {
