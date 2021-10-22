7a4426b49142692cf44b17813d68bb2801998c29
milka_deluxe <milagalko1@gmail.com>
2021-10-22 13:37:11 +0300

    Insertion done

--- RB tree/RB tree/main.cpp
+++ RB tree/RB tree/main.cpp
@@ -9,17 +9,17 @@ class Node {
     int key;
     string data;
     bool color;     // 0 - black    1 - red
-    Node *lChild,
-         *rChild,
+    Node *left,
+         *right,
          *parent;
     
-    Node(int key = -1, string data = "", Node *parent = nullptr, bool color = RED, Node *lChild = nullptr, Node *rChild = nullptr ) {
+    Node(int key = -1, string data = "", Node *parent = nullptr, bool color = RED, Node *left = nullptr, Node *right = nullptr ) {
         this->key = key;
         this->data = data;
         this->color = color;
-        this->lChild = lChild;
+        this->left = left;
         this->parent = parent;
-        this->rChild = rChild;
+        this->right = right;
     };
     
     friend class RBTree;
@@ -30,149 +30,132 @@ class RBTree {
     
 public:
     RBTree(): root(nullptr) {}
+    Node* sibling(Node*);
+    Node* grandparent(Node*);
+    Node* uncle(Node*);
     void insert(int, string);
-    void prettyPrint();
-    void printHelper(Node*, string, bool);
-    bool isLChild(Node*, Node*);
-    void leftRotate(Node*);     // Node which goes up
-    void rightRotate(Node*);    //
+    void deleteNode(Node*);
+    
+    bool isLeft(Node*);
+    bool isRight(Node*);
+    void leftRotate(Node*);     //   Node which goes up..
+    void rightRotate(Node*);    //.. should be an argument
+    void checkTree(Node*);
 };
 
-void RBTree::insert(int key, string data = "") {
-    if (root == nullptr) {
-        root = new Node(key, data, nullptr, BLACK);
-    }
-    else {
-        Node* current = root;
-        Node* prev = nullptr;
-        
-        while (current != nullptr) {
-            prev = current;
-            key < current->key ? current = current->lChild : current = current->rChild;
+void RBTree::deleteNode(Node* node) {
+    //CASE_1 node is BLACK & has a RED child
+    if (node->color == BLACK) {
+        Node* red;
+        if (node->left) {red = (node->left->color == RED) ? node->left : nullptr;}
+        else {
+            if (node->right) {red = (node->right->color == RED) ? node->right : nullptr;}
         }
         
-        current = new Node(key, data, prev);
-        prev->key > current->key ? prev->lChild = current : prev->rChild = current;
+        if (red) {
+            if (isLeft(red)) {
+                red->right = node->right;
+                if (node->right) {node->right->parent = red;}
+            }
+            red->parent = node->parent;
+            isLeft(node) ? node->parent->left = red : node->parent->right = red;
+            delete node;
+        }
+    }
+}
+
+
+void RBTree::insert(int key, string data = "") {
+    // CASE_1: first node - root (BLACK)
+    if (!root) {root = new Node(key, data, nullptr, BLACK);}
+    else {
+        Node* node = root;
+        Node* p = nullptr;   // parent
         
-        if (prev->color == RED) {
-            Node* uncle;
-            isLChild(prev, prev->parent) ? uncle = prev->parent->rChild : uncle = prev->parent->lChild;
+        while (node) {
+            p = node;
+            key < node->key ? node = node->left : node = node->right;
+        }
+        
+        node = new Node(key, data, p);
+        isLeft(node) ? p->left = node : p->right = node;
+        checkTree(node);
+    }
+}
+
+void RBTree::checkTree(Node* node) {
+        // CASE_2: p == RED
+    if (node->parent->color == RED) {
+        Node* p = node->parent;
+        Node* u = uncle(node);
+        Node* g = grandparent(node);
+        
+        // CASE_3: u == p == RED
+        if (u  &&  u->color == p->color == RED) {
+            u->color = p->color = BLACK;                            // recolor u&p to BLACK
+            if (g != root) {
+                g->color = RED;                                     // recolor g   to RED   (if not a root)
+                if (g->parent->color == RED) {checkTree(g);}
+            }
+        }
+        
+        // CASE_4: check g->l == p  && p->l == node (mirror)
+        else {
+            bool rotated = false;
+            if ((isLeft(node) && isRight(p))  ||  (isRight(node) && isLeft(p))) {
+                isRight(node) ? leftRotate(node) : rightRotate(node);
+                p = node->parent;
+                g = grandparent(node);
+                u = uncle(node);
+                rotated = true;
+            }
             
-            if (uncle != nullptr  &&  uncle->color == RED) {
-                uncle->color = prev->color = BLACK;
-                prev->parent != root ? prev->parent->color = RED : prev->parent->color = BLACK;
+            // CASE_5: p == RED,  u == BLACK
+            if (rotated) {
+                isLeft(node) ? rightRotate(node) : leftRotate(node);
+                p->color = RED;
+                node->color = BLACK;
             }
             else {
-                
-                if (!isLChild(prev, prev->parent)) {
-                    if (isLChild(current, prev)) {
-                        rightRotate(current);
-                        leftRotate(current);
-                        current->color = BLACK;
-                        current->lChild->color = RED;
-                    }
-                    else {
-                        leftRotate(prev);
-                        current->parent->color = BLACK;
-                        current->parent->lChild->color = RED;
-                    }
-                }
-                
-                else {
-                    if (!isLChild(current, prev)) {
-                        leftRotate(current);
-                        rightRotate(current);
-                        current->color = BLACK;
-                        current->rChild->color = RED;
-                    }
-                    else {
-                        leftRotate(prev);
-                        current->parent->color = BLACK;
-                        current->parent->rChild->color = RED;
-                    }
-                }
+                isLeft(node) ? rightRotate(p) : leftRotate(p);
+                p->color = BLACK;
+                g->color = RED;
             }
         }
     }
-    root->color = BLACK;
 }
 
 void RBTree::rightRotate(Node* node) {
-    Node* G = node->parent->parent; // G - future grand (used to be parent's parent)
-    Node* C = node->parent;         // C - future right child (used to be parent)
-    Node* currRChild = node->rChild;
+    Node* g = grandparent(node);  // future parent
+    Node* p = node->parent;       // future right child
     
-    G->rChild = node;
-    node->parent = G;
+    node->parent = g;
+    if (g) {isLeft(p) ? g->left = node : g->right = node;}
     
-    C->parent = node;
-    node->rChild = C;
+    p->left = node->right;
+    if (node->right) {node->right->parent = p;}
     
-    Node* current = node->rChild;
-    while (current != nullptr) {
-        current = current->lChild;
-    }
-    current = currRChild;
-    currRChild->parent = current->parent;
+    p->parent = node;
+    node->right = p;
 }
 
 void RBTree::leftRotate(Node* node) {
-    Node* G = node->parent->parent; // G - future grand (used to be parent's parent)
-    Node* C = node->parent;         // C - future left child (used to be parent)
-    Node* currLChild = node->lChild;
+    Node* g = grandparent(node);  // future parent
+    Node* p = node->parent;       // future left child
     
-    G->rChild = node;
-    node->parent = G;
+    node->parent = g;
+    if (g) {isLeft(p) ? g->left = node : g->right = node;}
     
-    C->parent = node;
-    node->lChild = C;
+    p->right = node->left;
+    if (node->left) {node->left->parent = p;}
     
-    Node* current = node->lChild;
-    while (current != nullptr) {
-        current = current->rChild;
-    }
-    current = currLChild;
-    currLChild->parent = current->parent;
+    p->parent = node;
+    node->left = p;
 }
 
-bool RBTree::isLChild(Node* current, Node* prev) {
-    bool IsLeftChild;
-    if (prev->key > current->key) {
-        IsLeftChild = true;
-        prev->lChild = current;
-    }
-    else {
-        IsLeftChild = false;
-        prev->rChild = current;
-    }
-    return IsLeftChild;
-}
 
-void RBTree::prettyPrint() {
-    if (root) {
-        printHelper(root, "", true);
-    }
-}
 
-void RBTree::printHelper(Node* root, string indent, bool last) {
-       if (root != nullptr) {
-       cout<<indent;
-       if (last) {
-          cout<<"R----";
-          indent += "     ";
-       } else {
-          cout<<"L----";
-          indent += "|    ";
-       }
-        
-       string sColor = root->color?"RED":"BLACK";
-       cout<<root->data<<"("<<sColor<<")"<<endl;
-       printHelper(root->lChild, indent, false);
-       printHelper(root->rChild, indent, true);
-    }
-}
 int main() {
-    
     RBTree bst;
     bst.insert(8, "8");
     bst.insert(18, "18");
@@ -182,6 +165,38 @@ int main() {
     bst.insert(25, "25");
     bst.insert(40, "40");
     bst.insert(80, "80");
-    bst.prettyPrint();
     return 0;
+}
+
+
+
+Node* RBTree::grandparent(Node* node) {
+    Node* g;
+    node->parent->parent ? g = node->parent->parent : g = nullptr;
+    return g;
+}
+
+Node* RBTree::uncle(Node* node) {
+    Node* u = nullptr;
+    if (grandparent(node)) {
+        isLeft(node->parent) ? u = grandparent(node)->right : u = grandparent(node)->left;
+    }
+    return u;
+}
+
+Node* RBTree::sibling(Node* node) {
+    Node* s;
+    Node* p = node->parent;
+    if (p) {s = isLeft(node) ? p->right : p->left;}
+    return s;
+}
+
+
+bool RBTree::isLeft(Node* node) {
+    if (node->parent->key > node->key) {return true;}
+    return false;
+}
+bool RBTree::isRight(Node* node) {
+    if (node->parent->key > node->key) {return false;}
+    return true;
 }
